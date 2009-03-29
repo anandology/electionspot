3c8caae4a6e487bd72013c90296808d0ea33158c
Anand Chitipothu <anandology@gmail.com>
2009-03-29 15:54:34 +0530

    add utility function storify

--- db.py
+++ db.py
@@ -1,6 +1,14 @@
 
 import web
 
+def storify(d):
+    if isinstance(d, list):
+        return [storify(x) for x in d]
+    elif isinstance(d, dict):
+        return web.storage((k, storify(v)) for k, v in d.items())
+    else:
+        return d
+
 def get_party(name):
     return web.storage()
 
@@ -10,5 +18,8 @@ def get_candidate(name):
 def get_state(name):
     return web.storage()
 
-def get_candidate(name):
-    return web.storage()
+def get_constituency(state, name):
+    return storify({
+        "id": "%s/%s" % (state, name),
+        "name": name
+    });
